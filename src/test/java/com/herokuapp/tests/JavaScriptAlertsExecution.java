cdcd875f9e9889932daa0f3d6d156cdf66c5e3b8
Sujay Sawant <sujayvsawant@gmail.com>
2020-08-15 14:18:54 +0530

    Added new tests to suite file.

---
+++ src/test/java/com/herokuapp/tests/JavaScriptAlertsExecution.java
@@ -0,0 +1,38 @@
+package com.herokuapp.tests;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.util.Map;
+
+import org.automation.base.BaseTest;
+import org.testng.annotations.Test;
+
+import com.herokuapp.pages.HomePage;
+import com.herokuapp.pages.JavaScripAlertsPage;
+
+public class JavaScriptAlertsExecution extends BaseTest {
+
+	@Test(dataProvider = "ExcelDataProvider")
+	public void testJavaScriptAlerts(Map<String, String> data) {
+		HomePage home = new HomePage();
+		JavaScripAlertsPage javaScripAlerts = home.javaScriptAlerts.click(JavaScripAlertsPage.class);
+		javaScripAlerts.jsAlert.click();
+		assertTrue(javaScripAlerts.javaScript.isPresent(), "Alert is not present");
+		assertEquals(javaScripAlerts.javaScript.getText(), data.get("Assertion1"), "Incorrect alert text");
+		javaScripAlerts.javaScript.accept();
+		assertEquals(javaScripAlerts.result.getText(), data.get("Assertion2"), "Incorrect result message");
+		javaScripAlerts.jsConfirm.click();
+		assertTrue(javaScripAlerts.javaScript.isPresent(), "Alert is not present");
+		assertEquals(javaScripAlerts.javaScript.getText(), data.get("Assertion3"), "Incorrect alert text");
+		javaScripAlerts.javaScript.dismiss();
+		assertEquals(javaScripAlerts.result.getText(), data.get("Assertion4"), "Incorrect result message");
+		javaScripAlerts.jsPrompt.click();
+		assertTrue(javaScripAlerts.javaScript.isPresent(), "Alert is not present");
+		assertEquals(javaScripAlerts.javaScript.getText(), data.get("Assertion5"), "Incorrect alert text");
+		javaScripAlerts.javaScript.enterText(data.get("Text"));
+		javaScripAlerts.javaScript.accept();
+		assertEquals(javaScripAlerts.result.getText(), "You entered: " + data.get("Text"), "Incorrect result message");
+	}
+
+}
